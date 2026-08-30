b102d3d5250d7dedad8c66946b2b169bcab31ab2
abdelbassetA12 <abdelbassetelhajiri2020@gmail.com>
2026-08-30 19:01:50 +0100

    updt

--- testnet/runner.js
+++ testnet/runner.js
@@ -2,31 +2,13 @@
 const axios = require("axios");
 const { process } = require("./trader");
 const { updateTrades, getActiveTrade } = require("./positionManager");
-async function getData(symbol) {
-  try {
-    const res = await axios.get(
-      "https://testnet.binance.vision/api/v3/time"
-    );
 
-    console.log("✅ BINANCE TESTNET:", res.status, res.data);
-
-    return [];
-  } catch (error) {
-    console.log(
-      "❌ BINANCE TESTNET:",
-      error.response?.status,
-      error.response?.data || error.message
-    );
-
-    throw error;
-  }
-}
 /*
   const res = await axios.get(
     `https://api.binance.com/api/v3/klines?symbol=${symbol}&interval=15m&limit=200`
        
   );*/
- /*
+ 
 async function getData(symbol) {
   const res = await axios.get(
   `https://testnet.binance.vision/api/v3/klines?symbol=${symbol}&interval=15m&limit=200`
@@ -41,7 +23,7 @@ async function getData(symbol) {
     close: +c[4]
   }));
 }
-*/
+ 
 async function run(symbols = ["BTCUSDT"]) {
   symbols.forEach(symbol => {
     setInterval(async () => {
