e515f333718be5f87b2b0bc2c69413079a05a6d0
abdelbassetA12 <abdelbassetelhajiri2020@gmail.com>
2026-09-03 19:58:52 +0100

    updt

--- server.js
+++ server.js
@@ -71,7 +71,8 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:3001",
+    //origin: "http://localhost:3001",
+    origin: "https://trading-server-ten.vercel.app",
     credentials: true
   })
 );
